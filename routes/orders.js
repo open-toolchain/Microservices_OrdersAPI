fa630eebaf9ab65a25c06f7863296531b8ed9312
huayuenh <48723418+huayuenh@users.noreply.github.com>
2020-12-03 09:59:00 +0000

    Revert "Add tests"

--- routes/orders.js
+++ routes/orders.js
@@ -1,16 +1,13 @@
 /*eslint-env node */
 /*globals cloudantService */
-var cloudant = (process.env.IS_TEST === "true") ? 
-	require('cloudant')(cloudantService.url): require('cloudant')(cloudantService.credentials.url);
+var cloudant = require('cloudant')(cloudantService.credentials.url);
 
 //Initiate the database.
 cloudant.db.create('orders', function(err/*, body*/) {
     if (!err) {
         console.log('Successfully created database!');
     } else {
-		if(process.env.IS_TEST !== "true") {
-			console.log("Database already exists.");
-		}
+        console.log("Database already exists.");
     }
  });
 
